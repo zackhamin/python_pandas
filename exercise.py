079f0dec859ea67fbab342f532ac0d5ddd6f59a0
zackhamin <ishaqhamin@gmail.com>
2021-01-07 12:41:42 +0000

    Exercises

--- exercise.py
+++ exercise.py
@@ -21,3 +21,15 @@ print(sal["BasePay"].mean())
 print("--------------Overtime pay--------------------")
 sal["OvertimePay"] = pd.to_numeric(sal["OvertimePay"], errors="coerce")
 print(sal["OvertimePay"].max())
+
+print("--------------Job Title--------------------")
+
+print(sal[sal["EmployeeName"] == "JOSEPH DRISCOLL"]["JobTitle"])
+
+print("--------------Total Pay incl Benefits--------------------")
+print(sal[sal["EmployeeName"] == "JOSEPH DRISCOLL"]["TotalPayBenefits"])
+
+print("--------------Total Pay incl Benefits--------------------")
+print(sal["TotalPayBenefits"].max())
+print(sal[sal["TotalPayBenefits"] == sal["TotalPayBenefits"].max()])
+
