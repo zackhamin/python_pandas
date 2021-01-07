8836a6330f93ee086ff380e0fe6c543cd1a4a5f8
zackhamin <ishaqhamin@gmail.com>
2021-01-07 11:09:19 +0000

    Exercises half way

--- exercise.py
+++ exercise.py
@@ -2,3 +2,22 @@ import pandas as pd
 
 salaries = pd.read_csv("../../archive/Salaries.csv")
 print(salaries)
+
+sal = pd.DataFrame(salaries)
+print(salaries)
+
+salaries_head = salaries.head()
+print(salaries_head)
+print("----------------------------------")
+
+entries = salaries.info()
+print(entries)
+
+print("------------Average Basepay----------------------")
+sal["BasePay"] = pd.to_numeric(sal["BasePay"], errors = "coerce")
+print(sal["BasePay"].mean())
+
+
+print("--------------Overtime pay--------------------")
+sal["OvertimePay"] = pd.to_numeric(sal["OvertimePay"], errors="coerce")
+print(sal["OvertimePay"].max())
